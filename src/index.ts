1868faea3164d5eee4cbc1e497663fdefaef3bff
Juli <schnullerpip@gmail.com>
2024-11-02 18:45:07 +0100

    add convenience types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { when } from './when'
 export { awaitable } from './awaitable'
 export { Observable } from './observable'
+export type { Satisfies } from './types/Satisfies'
+export type { Func } from './types/Func'
