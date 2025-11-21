fb41bd61846f1f81a56f0b35c68e6b3fb54ff989
Juli <schnullerpip@gmail.com>
2025-11-21 16:35:05 +0100

    improve retryable

--- src/retryable.ts
+++ src/retryable.ts
@@ -1,50 +1,37 @@
 import { sleep } from "./sleep";
 
 /**
- * 
+ *
  * Allows to execute a function that might fail.
  * The given handler will get access to a retry function that can be used to retry the given function.
  * The given handler will also get the current try count.
- * 
+ *
  * @example
  * ```ts
  * const result = retryable(({ retry, tryCount }) => {
  *     const result = doSomething()
  *     if (result.error === someCondition) {
  *         console.log(`try ${tryCount}`)
- *         retry()
+ *         retry({ backoffMs: 250 })
  *         // no code beneath retry will be executed
  *     }
  * })
  * ```
  * @param handler the function to execute that might fail
- * @param config the configuration for the retryable function
- * @param config.backoffMs the initial backoff in milliseconds
- * @param config.backoffIncrease the function to increase the backoff
+ * @param handler.tryCount the current try count
+ * @param handler.retry the function to initiate a retry
+ * @param handler.retry.backoffMs the backoff in milliseconds
+ * @param handler.retry.cause the cause of the retry
+ *
  * @returns the result of the function
  */
-export function retryable<T>(
-    handler: (context: { retry: (cause?: Error['cause']) => never; tryCount: number }) => T,
-    {
-        backoffMs = 200,
-        backoffIncrease = (backoffMs: number) => backoffMs * backoffMs
-    }: {
-        backoffMs?: number
-        backoffIncrease?: (backoffMs: number) => number
-    } = {}
-): T {
-    function retry(cause?: Error['cause']): never {
-        throw new Error('retry', { cause })
-    }
-
-    function isRetryable(error: unknown): boolean {
-        return error instanceof Error && error.message === 'retry'
-    }
-
-    function withRetry(tryCount: number, { backoffMs, backoffIncrease }: { backoffMs: number, backoffIncrease: (backoffMs: number) => number }): T {
+export function retryable<T>(handler: (context: { retry: RetryCommand; tryCount: number }) => T): T {
+    function withRetry(tryCount: number): T {
         try {
             const result = handler({
-                retry,
+                retry: ({ backoffMs }) => {
+                    throw new Retry(backoffMs)
+                },
                 tryCount,
             })
 
@@ -52,7 +39,7 @@ export function retryable<T>(
             if (result instanceof Promise) {
                 return result.catch((error) => {
                     if (isRetryable(error)) {
-                        return sleep(backoffMs).then(() => withRetry(tryCount + 1, { backoffMs: backoffIncrease(backoffMs), backoffIncrease }))
+                        return sleep(error.backoffMs).then(() => withRetry(tryCount + 1))
                     }
                     return Promise.reject(error)
                 }) as T
@@ -62,11 +49,26 @@ export function retryable<T>(
             return result
         } catch (error: unknown) {
             if (isRetryable(error)) {
-                return withRetry(tryCount + 1, { backoffMs: backoffIncrease(backoffMs), backoffIncrease })
+                return withRetry(tryCount + 1)
             }
             throw error
         }
     }
 
-    return withRetry(1, { backoffMs, backoffIncrease })
+    return withRetry(1)
+}
+
+//--- module private
+
+type RetryCommand = (args: { backoffMs: number }) => never
+
+function isRetryable(error: unknown): error is Retry {
+    return error instanceof Retry
+}
+
+class Retry extends Error {
+    constructor(readonly backoffMs: number) {
+        super('retry')
+        Object.setPrototypeOf(this, Retry.prototype)
+    }
 }
--- tests/retryable.test.ts
+++ tests/retryable.test.ts
@@ -6,10 +6,10 @@ describe('retryable', () => {
         let tryCount = 0
         const result = retryable(({ retry }) => {
             if (++tryCount < 3) {
-                retry()
+                retry({ backoffMs: 0 })
             }
             return tryCount
-        }, { backoffMs: 0 })
+        })
         expect(result).toEqual(3)
     })
 
@@ -17,12 +17,23 @@ describe('retryable', () => {
         let tryCount = 0
         const result = retryable(({ retry }) => {
             if (++tryCount < 3) {
-                retry()
+                retry({ backoffMs: 0 })
                 tryCount = 0 // should never be executed
             }
             return tryCount
-        }, { backoffMs: 0 })
+        })
         expect(result).toEqual(3)
+    })
+
+    it('works with a void function handler', () => {
+        let tryCount = 0
+        retryable(({ retry }) => {
+            if (++tryCount < 3) {
+                retry({ backoffMs: 0 })
+                tryCount = 0 // should never be executed
+            }
+        })
+        expect(tryCount).toEqual(3)
     })
 
     it('should provide the correct try count', () => {
@@ -30,32 +41,34 @@ describe('retryable', () => {
         retryable(({ retry, tryCount }) => {
             tries.push(tryCount)
             if (tryCount < 3) {
-                retry()
+                retry({ backoffMs: 0 })
             }
-        }, { backoffMs: 0 })
+        })
         expect(tries).toEqual([1, 2, 3])
     })
 
     it('works with async handlers - case success', async () => {
         let tryCount = 0
         const result = await retryable(async ({ retry }) => {
-            if (++tryCount < 3) {
-                retry()
+            if (++tryCount < 2) {
+                retry({ backoffMs: 0 })
                 tryCount = 0 // should never be executed
             }
             return tryCount
-        }, { backoffMs: 0 }).catch(() => {})
-        expect(result).toEqual(3)
+        })
+        expect(result).toEqual(2)
     })
 
     it('works with async handlers - case failure', async () => {
         let tryCount = 0
-        const result = await retryable(async ({ retry }) => {
-            if (++tryCount < 1) {
-                retry()
+        await retryable(async ({ retry }) => {
+            if (++tryCount < 2) {
+                retry({ backoffMs: 0 })
             }
             throw new Error('test')
-        }, { backoffMs: 0 }).catch(() => 10) // 'test' error can be catched and handled
-        expect(result).toEqual(10)
+        }).catch(() => {
+            tryCount++
+        }) // 'test' error can be catched and handled
+        expect(tryCount).toEqual(3)
     })
 })
